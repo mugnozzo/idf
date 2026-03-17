4d0747c007d4a44accff8a976378094da337bed7
Alek Mugnozzo <mugnozzo@gmail.com>
2026-03-17 17:40:42 +0100

    Added elements post logic

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,17 @@
 const host = "http://localhost:3000";
 
+const getTypesButton = document.getElementById("get-types-button");
+const getStatusesButton = document.getElementById("get-statuses-button");
+const getTagsButton = document.getElementById("get-tags-button");
+const getElementsButton = document.getElementById("get-elements-button");
+
+function listsClick () {
+  getTypesButton.click();
+  getStatusesButton.click();
+  getTagsButton.click();
+  getElementsButton.click();
+}
+
 // Setup clicks on GET buttons
 document.querySelectorAll(".get-button").forEach( function(value) {
   value.addEventListener("click", function () {
@@ -22,4 +34,29 @@ document.querySelectorAll(".get-button").forEach( function(value) {
       })
       .catch(error => console.error(error));
   });
+});
+
+// Element POST
+const postElementsButton = document.getElementById("post-elements-button");
+postElementsButton.addEventListener("click", function() {
+  const name = document.getElementById("post-elements-name").value;
+  const type = Number(document.getElementById("post-elements-type").value);
+  const status = Number(document.getElementById("post-elements-status").value);
+  const result = document.getElementById("post-elements-result");
+  apiRequest(host+"/api/elements", 'POST', {name, type, status})
+    .then(data => {
+      console.log(data);
+      result.innerHTML = "";
+      const table = document.createElement("table");
+      const tr = document.createElement("tr");
+      for (const field in data) {
+        const td = document.createElement("td");
+        td.innerHTML = data[field];
+        tr.appendChild(td);
+      }
+      table.appendChild(tr);
+      result.appendChild(table);
+      listsClick();
+    })
+    .catch(error => console.error(error));
 });
